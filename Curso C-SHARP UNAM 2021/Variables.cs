2c82ecd46e56c75fe728310997724be31378285d
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-06-30 09:22:45 -0500

    Declarando un entero y un booleano

---
+++ Curso C-SHARP UNAM 2021/Variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_C_SHARP_UNAM_2021 {
+    class Variables {
+        static void Main(string[] args) {
+            int myEntero = 5;
+            bool myBooleano = true;
+            Console.WriteLine(myBooleano);
+            Console.WriteLine(myEntero);
+        }
+    }
+}
