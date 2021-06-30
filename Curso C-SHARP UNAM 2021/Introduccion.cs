60bb87eae3cb4ca80291dfeecbec42451c5064be
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-06-30 09:11:49 -0500

    Agregar archivos de proyecto.

---
+++ Curso C-SHARP UNAM 2021/Introduccion.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Curso_C_SHARP_UNAM_2021 {
+    class Introduccion {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
