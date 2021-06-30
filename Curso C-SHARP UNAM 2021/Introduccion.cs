9997fd1f50b7cfbe6e01fcee7a89ae98faf9f77e
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-06-30 09:32:57 -0500

    Eliminando archivo de introduccion

--- Curso C-SHARP UNAM 2021/Introduccion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Curso_C_SHARP_UNAM_2021 {
-    class Introduccion {
-        static void Main(string[] args) {
-            Console.WriteLine("Introduccion al lenguaje de c#");
-        }
-    }
-}
