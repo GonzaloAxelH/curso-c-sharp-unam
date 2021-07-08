16728ec5623eff0790e0aa845c08f0e8e9914dcd
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-08 16:52:28 -0500

    Modificando estructuras

--- Curso C-SHARP UNAM 2021/Variables.cs
+++ Curso C-SHARP UNAM 2021/Variables.cs
@@ -3,22 +3,7 @@
 namespace Curso_C_SHARP_UNAM_2021 {
     class Variables {
         static void Main(string[] args) {
-            /*-------------------------Declaracion Implicita -----------------------------*/
-            var myNombre ="Diego";//correcto
-            myNombre = "gonzalo";//correcto
-            myNombre = 5;//error
-
-
-            //obligados a inicializar
-
-            var myVar;//error
-            myVar = 5;
-
-            var myVar2 = 8;
-
-            Console.WriteLine(myNombre);
-    
-
+     
         }
     }
 }
