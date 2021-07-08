cd5c9ab374ba56da806396774626b1275f95ed63
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-08 18:19:21 -0500

    Guadando nueva estructura

--- Curso C-SHARP UNAM 2021/Chapter-I/EstructuraPrograma/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Curso_C_SHARP_UNAM_2021.Chapter_I.EstructuraPrograma {
-    class Class1 {
-    }
-}
---
+++ Curso C-SHARP UNAM 2021/Chapter-I/EstructuraPrograma/NameSpaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CURSO.Chapter_I.EstructuraPrograma {
+    /* . . . */
+}
+
+namespace CURSO.Chapter_I.Other_Thema{
+    /* . . . */
+}
+
+/*
+  OBSERVACIONES
+ *Se puede crear muchos namespace
+ *Cuando se crea carpetas dentro de c#, lo interpreta como un namespace
+ *En un namespace no se pueden repetir las clases
+ */
