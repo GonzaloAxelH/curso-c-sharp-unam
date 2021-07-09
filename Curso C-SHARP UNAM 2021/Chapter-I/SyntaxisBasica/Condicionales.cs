9f25ae865313d4f03e106ee55a41f39ec381d03c
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-09 09:12:37 -0500

    Agregando ejemplo de condicionales

--- Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/Condicionales.cs
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/Condicionales.cs
@@ -5,6 +5,18 @@ using System.Text;
 namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisBasica {
     class Condicionales {
 
-        public static void inicio() { }
+        public static void inicio() {
+            //ejemplo de if else if 
+            var tamano = decimal.Parse(Console.ReadLine());
+            if (tamano < 0)
+                Console.WriteLine($"Tamano Negativo: {tamano}");
+            else if (tamano > 1000)
+                Console.WriteLine($"Tamano grande: {tamano}");
+            else {
+                Console.WriteLine($"Tamano aceptado: {tamano}");
+                Console.WriteLine($"Area: {tamano * tamano}");
+            }
+
+        }
     }
 }
