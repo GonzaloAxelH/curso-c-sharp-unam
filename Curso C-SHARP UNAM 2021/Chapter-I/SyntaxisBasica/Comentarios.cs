541d77c454e9bdbe02996531225d80a8e9185bb6
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-08 22:58:28 -0500

    Agregando mas temas

--- Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/Comentarios.cs
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/Comentarios.cs
@@ -4,6 +4,20 @@ using System.Text;
 
 namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisBasica {
     class Comentarios {
-        public static void inicio() { }
+        public static void inicio() {
+
+            //esto es un comentario :)
+            // los comentario con dos barras invertidas
+            //ejemplo suma de número
+            int num1 = 5;//declarando un número entero
+            int num2 = 6;
+            int suma = num1 + num2; //operación de la suma
+            Console.WriteLine(suma);//imprimiendo la suma
+            //también los comentarios de múltiples líneas            
+            /*           
+                Cualquier texto ..
+            */
+
+        }
     }
 }
--- Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/DeclaracionVariables.cs
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/DeclaracionVariables.cs
@@ -5,6 +5,19 @@ using System.Text;
 namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisBasica {
     class DeclaracionVariables {
 
-        public static void inicio() { }
+        public static void inicio() {
+            var myNombre = "Diego";//correcto
+            myNombre = "gonzalo";//correcto
+           //myNombre = 5;//error
+            //obligados a inicializar
+         
+            //var myVar;
+            //myVar = 5;
+
+            var myVar2 = 8;//correcto
+
+            Console.WriteLine(myNombre);
+            Console.WriteLine(myVar2);
+        }
     }
 }
