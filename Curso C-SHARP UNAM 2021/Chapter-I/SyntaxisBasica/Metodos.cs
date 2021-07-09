81278d084b2d6e00d0db2bef9c3c110dc5f9ed6d
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-09 09:10:21 -0500

    Agregnado ejemplo de de metodos

---
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisBasica/Metodos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisBasica {
+    class Metodos {
+        public static void inicio() {
+            //Metodo que no devuelve nada 
+            void imprimirAlgo() {
+                Console.WriteLine("Algo :v");
+            }
+            //metodo que si entrega un tipo de dato
+            DateTime getFecha() {
+                return DateTime.Today;// se especifica return para devolver
+            }
+
+            //Ejecutamos los metodos
+            imprimirAlgo();//de esta manera llamamos al metodo
+            Console.WriteLine(getFecha());//llamamos al metodo dentro  para imprimir lo retornado
+
+            //metodos con parametros
+            int suma(int num1,int num2) {
+                return num1 + num2;
+            }
+
+        }
+    }
+
+}
