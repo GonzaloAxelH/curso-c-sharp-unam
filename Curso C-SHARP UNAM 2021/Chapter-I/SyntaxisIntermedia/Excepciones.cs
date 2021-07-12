746da2eefe2e27e48155f15f57d4dba6bc8e64e4
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-11 21:28:58 -0500

    Agregando ejemplo de division entre zero

---
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisIntermedia/Excepciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisIntermedia {
+    class Excepciones {
+
+        
+        public static void inicio() {
+    
+         double divisionTwoNumbers(double x,double y) {
+                if (y == 0) {
+                    throw new DivideByZeroException();
+                }
+                return x / y;
+            }
+
+            double a = 98;
+            double b = 0;
+            double resutl;
+            try {
+                resutl = divisionTwoNumbers(a, b);
+                Console.WriteLine("{0} dividido por {1} = {2} ", a, b, resutl);
+
+            } catch (DivideByZeroException) {
+                Console.WriteLine("No se puede dividir entre zero");
+            }
+        }
+    }
+}
