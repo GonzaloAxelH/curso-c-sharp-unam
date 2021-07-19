6ea53451bb0e852d8339781fee8c84485d614f40
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-19 12:42:58 -0500

    Creando tema de structs y genericos

---
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisIntermedia/Genericos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisIntermedia {
+    class Genericos {
+        public static void inicio() {
+            AlamacenaObjetos archivo = new AlamacenaObjetos(4);
+            archivo.agregar("Gonzalo");
+            archivo.agregar("Juan");
+            archivo.agregar("Gorge");
+            archivo.agregar("Antonio");
+            String nombrePersona = (String)archivo.getElemento(2);//necesidad de castear 
+            //alacenamos objetos de tipo empleados
+            AlamacenaObjetos empleados = new AlamacenaObjetos(3);
+            empleados.agregar(new Empleado(122));
+            empleados.agregar(new Empleado(1222));
+            String nombreEmpleado = (String)empleados.getElemento(1);
+            Console.WriteLine(nombreEmpleado);
+        }
+    }
+
+
+    public class AlamacenaObjetos {
+        private Object[] datosElemento;
+        private int i = 0;
+        public AlamacenaObjetos(int z) {
+            datosElemento = new Object[z];
+        }
+            public void agregar(Object obj) {
+                datosElemento[i] = obj;
+                i++;
+            }
+        public Object getElemento(int position) {
+            return datosElemento[position];
+        }
+
+    }
+
+    class Empleado {
+        private double salario;
+        public Empleado(double sal) {
+            salario = sal;
+        }
+        public double getSalario() {
+            return salario;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Curso C-SHARP UNAM 2021/Chapter-I/SyntaxisIntermedia/Struct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_C_SHARP_UNAM_2021.Chapter_I.SyntaxisIntermedia {
+    class Struct {
+        public static void inicio() {
+           //ejemplo 1
+            Customer c1 = new Customer();
+       
+            Console.WriteLine(c1.ID);
+
+            //ejemplo 2
+            Coords coord1;
+            coord1.x = 10;
+            coord1.y = 12;
+            Console.WriteLine("Coords 1:");
+            Console.WriteLine("x = {0}, y = {1}", coord1.x, coord1.y);
+
+
+        }   
+
+        public struct Customer { 
+            public int ID;
+            public string Name;
+            public Customer(int customerID,string customerName) {
+                ID = customerID;
+                Name = customerName;
+            }
+        }
+        public struct Coords {
+            public int x, y;
+            public Coords(int p1,int p2) {
+                x = p1;
+                y = p2;
+            }
+
+        }
+
+    }
+}
